6b4fb7f53de19f6fcae9c0e4b60f440e079f4107
vitaliymelamud <melamudv@gmail.com>
2021-10-15 16:16:45 +0300

    add recursive view of tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit} from '@angular/core';
 import {ITree} from '../shared/interface';
 import {DataService} from '../core/data.service';
 import {Subscription} from 'rxjs/Subscription';
@@ -9,12 +9,25 @@ import {Subscription} from 'rxjs/Subscription';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService, private elementRef: ElementRef) {}
   subscription: Subscription;
   tree: ITree[]= [];
   ngOnInit() {
     this.subscription = this.dataService.getTree()
-        .subscribe((tree: ITree[]) => {this.tree = tree});
+        .subscribe((tree: ITree[]) => {
+          this.tree = tree
+          this.recursive(this.tree);
+        });
+  }
+  recursive(tree){
+    tree.forEach(node =>{
+      let treeRecursive = this.elementRef.nativeElement.querySelector('.tree-recursive');
+      treeRecursive.insertAdjacentHTML('beforeend', `<div>${node.title}</div>`);
+      if(!node.level){
+        return node;
+      }
+      this.recursive(node.level)
+    })
   }
   ngOnDestroy() {
     this.subscription.unsubscribe()
